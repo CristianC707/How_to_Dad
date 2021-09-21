d9485f29caf0cbe09985e5d395d54a7926cfc5dd
Bugs Bunny <Cristian.e.cuevas707@gmail.com>
2021-09-21 17:50:00 -0400

    Added api and search option

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useQuery } from '@apollo/client';
 
 import ThoughtList from '../components/ThoughtList';
@@ -8,8 +8,33 @@ import { QUERY_THOUGHTS } from '../utils/queries';
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_THOUGHTS);
+  const [searchTerm, setSearchTerm] = useState({
+    search: ""
+  });
   const thoughts = data?.thoughts || [];
+  const handleChange = (event) => {
+    const {name, value} = event.target
+    setSearchTerm({
+      ...searchTerm,[name]: value
+    })
+  }
+  const onClickMe = async () => {
+    console.log(searchTerm.search)
 
+    try {
+      const response = await fetch("https://icanhazdadjoke.com/search?term=" + searchTerm.search   , {
+        headers: {
+          Accept: "Application/Json"
+        }
+      })
+      console.log(response)
+      const data = await response.json()
+      console.log(data) 
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <main>
       <div className="flex-row justify-center">
@@ -23,10 +48,14 @@ const Home = () => {
           {loading ? (
             <div>Loading...</div>
           ) : (
-            <ThoughtList
-              thoughts={thoughts}
-              title="Some Feed for Thought(s)..."
-            />
+            // <ThoughtList
+            //   thoughts={thoughts}
+            //   title="Some Feed for Thought(s)..."
+            // />
+            <div>
+            <input name= "search" onChange= {handleChange} value= {searchTerm.search}/>
+            <button onClick={onClickMe}> Click Me! </button>
+            </div>
           )}
         </div>
       </div>
